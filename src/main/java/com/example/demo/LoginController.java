9577e01090b589e0a2d7227efff781f96694b333
ASzazX <prokopiszynwiktor@gmail.com>
2024-07-12 01:23:30 +0200

    Initial

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -14,7 +11,7 @@ public class LoginController {
     public String test() {
         return "test";
     }
-    @RequestMapping("/login")
+    @PostMapping("/login")
     public String login(@RequestBody User user, HttpSession session) {
 
         String correctLogin = "admin";
@@ -28,7 +25,6 @@ public class LoginController {
             attempts = 0;
         }
         if (username.equals(correctLogin) && password.equals(correctPassword)) {
-            attempts=0;
             session.setAttribute("loginAttempts", 0);
             return "success";
         }
