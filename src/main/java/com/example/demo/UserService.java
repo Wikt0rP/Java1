1deb56bcd69184ad139992201b22968ce4a4bc13
wiktor <prokopiszynwiktor@gmail.com>
2024-07-13 16:22:58 +0200

    fixes

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
     private UserRepository userRepository;
 
     public Boolean unblockUser(User user, LoginRequest unblockRequest) {
         if(user.getPassword().equals(unblockRequest.getPassword())){
             user.setIsBlocked(false);
             userRepository.save(user);
+
             return true;
         }else{
             return false;
@@ -17,7 +20,7 @@ public class UserService {
     }
 
     public void blockUser(User user) {
-        user.setIsBlocked(true);
-        userRepository.save(user);
+       user.setIsBlocked(true);
+       userRepository.save(user);
     }
 }
