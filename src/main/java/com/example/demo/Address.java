a5fb7357ad6c30f63808020deaf87d0c51db4631
wiktor <prokopiszynwiktor@gmail.com>
2024-07-16 23:49:47 +0200

    some notes etc.

---
+++ src/main/java/com/example/demo/Address.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ip;
+
+    public Address(String ip) {
+        this.ip = ip;
+    }
+}
